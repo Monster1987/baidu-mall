91cf2f2dc6a7762c4d66f54f06d3cc1e3fc3ece2
zhangchangfu <zhangchangfu1987@163.com>
2020-03-14 13:00:47 +0800

    项目依赖及模块搭建

---
+++ mall-admin/src/test/java/com/baidu/mall/PmsDaoTest.java
@@ -0,0 +1,4 @@
+package com.baidu.mall;
+
+public class PmsDaoTest {
+}
---
+++ test/src/main/java/com/baidu/mall/test/Controller.java
@@ -0,0 +1,15 @@
+package com.baidu.mall.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+    @RequestMapping(value = {"/",""})
+    public String hellTask(@Value("${myName}")String username ){
+
+        return "hello  my current myName is " + username;
+    }
+}
